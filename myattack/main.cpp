c6b1763979680b802e1b799b6b5f6e163ee0200e
kyousuke4k <none>
2019-10-31 10:57:25 +0900

    add login

--- myattack/main.cpp
+++ myattack/main.cpp
@@ -1,4 +1,29 @@
 #include <iostream>
+#include <string>
+using namespace std;
+bool login(string typepas) {
+	if (typepas == "fire")return true;
+	return false;
+}
+void MTAmenu() {
+
+}
+
 int main(void) {
+	string typepas;
+	while (true) {
+		cout << "typepas..." << endl;
+		cin >> typepas;
+		if (login(typepas)) {
+			cout << "success." << endl;
+			break;
+		}
+		else {
+			cout << "access failed." << endl;
+		}
+	}
+
+	cin.get();
+	cin.get();
 	return 0;
 }
